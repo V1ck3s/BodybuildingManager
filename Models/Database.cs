b8eb4d623b04da4101960ea93cdbff9987407cd3
Vickes <Vickes>
2022-07-23 17:38:33 +0200

    feat: Adding dockerfile, clean database, database is now at root folder.

--- Models/Database.cs
+++ Models/Database.cs
@@ -18,15 +18,17 @@ namespace BodybuildingManager.Models.Database
 
         public DatabaseContext()
         {
-            var folder = Environment.SpecialFolder.LocalApplicationData;
-            var path = Environment.GetFolderPath(folder);
+            var path = Environment.CurrentDirectory;
             DbPath = System.IO.Path.Join(path, "database.db");
         }
 
         // The following configures EF to create a Sqlite database file in the
         // special "local" folder for your platform.
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite($"Data Source={DbPath}");
+        protected override void OnConfiguring(DbContextOptionsBuilder options){
+            options.UseSqlite($"Data Source={DbPath}");
+
+        }
+            
     }
 
     public class Programme
